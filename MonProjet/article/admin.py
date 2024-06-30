7885aa4b714d12d4df4162ec853a63c8ee8dfcb8
Alfredveve <128948530+Alfredveve@users.noreply.github.com>
2024-06-30 06:52:52 +0000

    Partie : 3
    Création de class AdminArticle pour une affichage des articles sous forme d'un tableau de liste dans le fichier admin.py

--- MonProjet/article/admin.py
+++ MonProjet/article/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from . models import Articles
 
+class AdminArticles(admin.ModelAdmin):
+    list_display = ['name', 'description', 'price', 'actif', 'slug', 'image']
 
-admin.site.register(Articles)
+
+admin.site.register(Articles, AdminArticles)
